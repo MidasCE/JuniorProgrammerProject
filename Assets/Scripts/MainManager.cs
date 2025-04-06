bc18cd599d8eccc4c4c93061339325820fbee533
pattarapol_tiensermpong <midaskmitl@gmail.com>
2025-04-06 12:56:08 +0700

    Scene transition, data persistance

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+    
+    public Color TeamColor;
+    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    
+}
